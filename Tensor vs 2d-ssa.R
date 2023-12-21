f5c4a76c0658b729079e293c31030b0bc3c63ba6
NikitaKhromovSPBU <129770345+NikitaKhromovSPBU@users.noreply.github.com>
2023-12-21 16:14:31 +0300

    Added a few cases

--- Tensor vs 2d-ssa.R
+++ Tensor vs 2d-ssa.R
@@ -7,45 +7,75 @@ set.seed(1)
 N <- 71
 
 signal1 <- 30 * cos(2 * pi * 1:N / 12)
-# signal2 <- 20 * cos(2 * pi * 1:N / 12) # case 1
-# r <- 2
-# r3 <- 1
-# signal2 <- 20 * cos(2 * pi * 1:N / 12 + pi / 4) # case 2
+
+signal2 <- 20 * cos(2 * pi * 1:N / 12)
+signal3 <- 25 * cos(2 * pi * 1:N / 12)
+signal4 <- 35 * cos(2 * pi * 1:N / 12)
+r <- 2
+r3 <- 1
+rd <- 4
+# signal2 <- 20 * cos(2 * pi * 1:N / 12 + pi / 4)
+# signal3 <- 25 * cos(2 * pi * 1:N / 12)
+# signal4 <- 35 * cos(2 * pi * 1:N / 12)
 # r <- 2
 # r3 <- 2
-signal2 <- 20 * cos(2 * pi * 1:N / 8 + pi / 4) # case 3
-r <- 4
-r3 <- 2
+# rd <- 4
+# signal2 <- 20 * cos(2 * pi * 1:N / 8 + pi / 4)
+# signal3 <- 25 * cos(2 * pi * 1:N / 12)
+# signal4 <- 35 * cos(2 * pi * 1:N / 12)
+# r <- 4
+# r3 <- 2
+# rd <- 8
+# signal2 <- 20 * cos(2 * pi * 1:N / 12 + pi / 4)
+# signal3 <- 25 * cos(2 * pi * 1:N / 12 + pi / 2)
+# signal4 <- 35 * cos(2 * pi * 1:N / 12 + 3 * pi / 4)
+# r <- 2
+# r3 <- 2
+# rd <- 4
+# signal2 <- 20 * cos(2 * pi * 1:N / 8 + pi / 4)
+# signal3 <- 25 * cos(2 * pi * 1:N / 12 + pi / 2)
+# signal4 <- 35 * cos(2 * pi * 1:N / 8 + 3 * pi / 4)
+# r <- 4
+# r3 <- 4
+# rd <- 8
+# signal2 <- 20 * cos(2 * pi * 1:N / 8 + pi / 4)
+# signal3 <- 25 * cos(2 * pi * 1:N / 10 + pi / 2)
+# signal4 <- 35 * cos(2 * pi * 1:N / 14 + 3 * pi / 4)
+# r <- 8
+# r3 <- 4
+# rd <- 12
 
-series.number <- 2
+signal <- cbind(signal1, signal2, signal3, signal4)
 
-signal <- cbind(signal1, signal2)
+series.number <- 4
+
 R <- 100
 
 sigma <- 5
-Ls <- c(12, 24, 36, 48, 60)
-# Ls <- seq(36, 50, 2)
+# Ls <- c(12, 24, 36, 48, 60)
+Ls <- seq(36, 50, 2)
 
 mssa.errors <- function(Ls) {
   noise1 <- rnorm(N, sd = sigma)
-  # noise1 <- arima.sim(n = N, model = list(ar = 0.5), sd = sqrt(5))
-  # noise1 <- arima.sim(n = N, model = list(ar = 0.9), sd = sqrt(5))
   noise2 <- rnorm(N, sd = sigma)
-  # noise2 <- arima.sim(n = N, model = list(ar = 0.5), sd = sqrt(5))
-  # noise2 <- arima.sim(n = N, model = list(ar = 0.9), sd = sqrt(5))
+  noise3 <- rnorm(N, sd = sigma)
+  noise4 <- rnorm(N, sd = sigma)
   f1 <- signal1 + noise1
   f2 <- signal2 + noise2
-  f <- cbind(f1, f2)
-  f.list <- list(f1, f2)
-  err.rec <- list(mat = numeric(length(Ls)), tens = numeric(length(Ls)))
-  names(err.rec$mat) <- Ls; names(err.rec$tens) <- Ls
+  f3 <- signal3 + noise3
+  f4 <- signal4 + noise4
+  f <- cbind(f1, f2, f3, f4)
+  f.list <- list(f1, f2, f3, f4)
+  err.rec <- list(mat = numeric(length(Ls)), mat_2d = numeric(length(Ls)), tens = numeric(length(Ls)))
+  names(err.rec$mat) <- Ls; names(err.rec$tens) <- Ls; names(err.rec$mat_2d) <- Ls
   for (l in seq_along(Ls)) {
     L <- Ls[l]
-    # s <- ssa(f, L = L, kind = "mssa")
-    s <- ssa(f, L = c(L, series.number / 2), kind = "2d-ssa")
-    # rec <- reduce(reconstruct(s, groups = list(1:r))[[1]], cbind)
-    rec <- reconstruct(s, groups = list(1:r))[[1]]
-    err.rec$mat[l] <- mean((rec - signal)^2)
+    s1 <- ssa(f.list, L = L, kind = "mssa")
+    s2 <- ssa(f, L = c(L, series.number / 2), kind = "2d-ssa")
+    rec1 <- reduce(reconstruct(s1, groups = list(1:r))[[1]], cbind)
+    rec2 <- reconstruct(s2, groups = list(1:rd))[[1]]
+    err.rec$mat[l] <- mean((rec1 - signal)^2)
+    err.rec$mat_2d[l] <- mean((rec2 - signal)^2)
 
     f.hankel <- lapply(f.list, hankel, L)
     mat.f <- reduce(f.hankel, cbind)
@@ -60,8 +90,9 @@ mssa.errors <- function(Ls) {
 
 mres <- replicate(R, mssa.errors(Ls))
 
-cat("RMSE for 2D-SSA:\n", sqrt(rowMeans(simplify2array(mres[1,]))), "\n")
-cat("RMSE for HOSVD MSSA:\n", sqrt(rowMeans(simplify2array(mres[2,]))), "\n")
+cat("RMSE for MSSA:\n", sqrt(rowMeans(simplify2array(mres[1,]))), "\n")
+cat("RMSE for 2D-SSA:\n", sqrt(rowMeans(simplify2array(mres[2,]))), "\n")
+cat("RMSE for HOSVD MSSA:\n", sqrt(rowMeans(simplify2array(mres[3,]))), "\n")
 
 # Значимость
 # t.test(simplify2array(mres[1,])[6,], simplify2array(mres[2,])[5,], paired = TRUE, alternative = "greater")
